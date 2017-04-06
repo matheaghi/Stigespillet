de841276479b4a56ef7b10ceaced30a28132336c
randigu <randi.gustad@gmail.com>
2017-04-06 13:34:17 +0200

    Har laget gettere

--- Stigespillet/src/stigespillet/Player.java
+++ Stigespillet/src/stigespillet/Player.java
@@ -2,4 +2,30 @@ package stigespillet;
 
 public class Player {
 
+	private String name;
+	private int playerNr;
+	private Rute iRute;
+	private boolean pause = false;
+	
+	
+	public Player(){
+		
+		
+		
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public int getPlayerNr(){
+		return this.playerNr;
+	}
+	
+	public Rute getIRute(){
+		return this.iRute;
+	}
+	public boolean getPause(){
+		return this.pause;
+	}
 }
