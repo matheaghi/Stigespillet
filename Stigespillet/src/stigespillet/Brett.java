b19454996cdb8beeff1be0a2d8a836635a44e60e
randigu <randi.gustad@gmail.com>
2017-04-06 18:43:41 +0200

    Har skreve litt i brett

--- Stigespillet/src/stigespillet/Brett.java
+++ Stigespillet/src/stigespillet/Brett.java
@@ -1,6 +1,25 @@
 package stigespillet;
 
+import java.util.ArrayList;
+
+
 public class Brett {
 	
-	//Her skal det skrives inn fer etter hvert
-}
+	private int sinTur;
+	private ArrayList<Rute> brettet;
+	private List<Player> spillere = new ArrayList<>();
+	private boolean isGameOver = false;
+	
+	
+	public Brett(int antallSpillere){
+		this.sinTur = 1;
+		Player sp;
+		for (int i = 0; i < antallSpillere; i++){
+			sp = new Player(name, playerNr);
+			spillere.add(sp);
+		}
+	}
+	
+	
+	
+}	
