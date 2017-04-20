778f842126b7b8dc4b734ed8c7cf361da26eea3e
Mathea <mathea.hildre@gmail.com>
2017-04-20 09:48:30 +0200

    blabla

--- Stigespillet/src/stigespillet/Player.java
+++ Stigespillet/src/stigespillet/Player.java
@@ -1,16 +1,23 @@
 package stigespillet;
 
-public class Player {
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Player extends Entity{
 
 	private String name;
 	private int playerNr;
 	private Rute iRute /* index, ikke nummeret på brettet*/;
 	private boolean pause = false;
+	private Image icon = Assets.grin;
 	
+	private Game game;
 	
-	public Player(String name, int playerNr){
+	public Player(String name, int playerNr, float x, float y, Game game){
+		super(x, y);
 		this.name = name;
 		this.playerNr = playerNr;
+		this.game = game;
 	}
 	
 	public String getName(){
@@ -35,6 +42,23 @@ public class Player {
 	
 	public void setPause(boolean some){
 		pause = some;
+	}
+
+	@Override
+	public void tick() {
+		if(game.getKeyManager().up)
+			y -= 3;
+		if(game.getKeyManager().down)
+			y += 3;
+		if(game.getKeyManager().left)
+			x -= 3;
+		if(game.getKeyManager().right)
+			x += 3;
+	}
+
+	@Override
+	public void render(Graphics g) { 
+		g.drawImage(icon, (int)x, (int)y, null);
 	}
 	
 }
